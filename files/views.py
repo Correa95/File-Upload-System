5cf32bf088ae4ab276c72c3be7c8f2ef502f9ea0
Correa95 <mathewcorrea19@gmail.com>
2025-02-23 21:20:33 -0500

    testing routes

--- files/views.py
+++ files/views.py
@@ -10,36 +10,54 @@ from .forms import UploadForm
 
 
 # Create your views here.
-@api_view(['GET', 'PUT', 'DELETE'])
-def files(request,  file_id, format = None):
+@api_view(['GET', 'POST'])
+def files(request, format=None):
     if request.method == 'GET':
-        serializer = FileSerializer(data)
-        return Response({'file': serializer.data}, status=status.HTTP_200_OK)
-
-
-    if request.method == 'GET':
-        serializer = FileSerializer(data)
-        return Response({'file': serializer.data}, status=status.HTTP_200_OK)
-    
-
-    elif request.method == 'PATCH':
-        serializer = FileSerializer(data, data=request.data)
+        data = File.objects.all()
+        serializer = FileSerializer(data, many=True)
+        return Response({'files': serializer.data})
+        
+    elif request.method == 'POST':
+        serializer = FileSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
 
     
-    elif request.method == 'PUT':
-        serializer = FileSerializer(data, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def files(request,  file_id, format = None):
+#     if request.method == 'GET':
+#         serializer = FileSerializer(data)
+#         return Response({'file': serializer.data}, status=status.HTTP_200_OK)
 
-    elif request.method == 'DELETE':
-        File.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+#     if request.method == 'GET':
+#         serializer = FileSerializer(data)
+#         return Response({'file': serializer.data}, status=status.HTTP_200_OK)
+    
+
+#     elif request.method == 'PATCH':
+#         serializer = FileSerializer(data, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+#     elif request.method == 'PUT':
+#         serializer = FileSerializer(data, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == 'DELETE':
+#         File.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
