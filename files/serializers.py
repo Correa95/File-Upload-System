0e697b4e88b19758fcf50d78ef5b5e0964d87867
Correa95 <mathewcorrea19@gmail.com>
2025-03-05 11:55:47 -0500

    aws install

--- files/serializers.py
+++ files/serializers.py
@@ -12,19 +12,19 @@ class FileSerializer(serializers.ModelSerializer):
         return File.objects.create(use=self.context["request"].user, **validated_data)
     
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = "__all__"
-#         write_only_fields = ("password")
-#         read_only_fields = ("id",)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+        write_only_fields = ("password")
+        read_only_fields = ("id",)
 
-#     def create(self, validated_data):
-#         user = User.objects.create( username = validated_data["username"],
-# email = validated_data["email"])
+    def create(self, validated_data):
+        user = User.objects.create( username = validated_data["username"],
+email = validated_data["email"])
         
-#         user.set_password(validated_data["password"])
-#         user.save()
+        user.set_password(validated_data["password"])
+        user.save()
         return user
 
 
