20a5a8646597dc4d37f618e6e012ce9631eb0ff7
Correa95 <mathewcorrea19@gmail.com>
2025-03-06 10:45:19 -0500

    working with aws

--- mario/settings.py
+++ mario/settings.py
@@ -83,8 +83,8 @@ SIMPLE_JWT = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
