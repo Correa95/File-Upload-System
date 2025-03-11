6d22f910839011eaa548239b13f7b83e64e832ca
Correa95 <mathewcorrea19@gmail.com>
2025-03-10 21:02:53 -0400

    initial commit

--- mario/settings.py
+++ mario/settings.py
@@ -207,10 +207,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = 'static'
+STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 
-# # Default primary key field type
-# # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
