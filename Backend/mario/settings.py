d08d555de4a642fe7727ea297a7c105f6c3bb50e
Correa95 <mathewcorrea19@gmail.com>
2025-05-19 21:00:17 -0400

    added the domain name to cors

--- Backend/mario/settings.py
+++ Backend/mario/settings.py
@@ -169,8 +169,8 @@ print("connection details:", DATABASES)
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
-    "http://localhost:5173",
-    "http://localhost"
+    "http://mathewcoolwebsite.com",
+    "https://mathewcoolwebsite.com"
     #whatever frontend URL you are using
 ]
 
