1f51db1c7d0d2a3e89b4306ea9eb1ceb689acdef
Correa95 <mathewcorrea19@gmail.com>
2025-05-21 11:01:34 -0400

    created a build file

--- Backend/mario/settings.py
+++ Backend/mario/settings.py
@@ -17,17 +17,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dg1ipt!zn!rtr)=l84u*gkcj4q0_z-vjqw5q^1!4%#fprqy4wv'
+# SECRET_KEY = 'django-insecure-dg1ipt!zn!rtr)=l84u*gkcj4q0_z-vjqw5q^1!4%#fprqy4wv'
 # Django project settings.py
 
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
-    "UPDATE_LAST_LOGIN": False,
+      'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
 
     "ALGORITHM": "HS256",
     "SIGNING_KEY": SECRET_KEY,
@@ -37,16 +37,24 @@ SIMPLE_JWT = {
     "JSON_ENCODER": None,
     "JWK_URL": None,
     "LEEWAY": 0,
+    
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': os.environ.get('SECRET_JWT', SECRET_KEY),
 
     "AUTH_HEADER_TYPES": ("Bearer",),
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
-    "USER_ID_FIELD": "id",
-    "USER_ID_CLAIM": "user_id",
-    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
 
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+     "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+
+ "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
     "TOKEN_TYPE_CLAIM": "token_type",
     "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+   
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+   
 
     "JTI_CLAIM": "jti",
 
@@ -61,20 +69,20 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'UPDATE_LAST_LOGIN': False,
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+#     'ROTATE_REFRESH_TOKENS': True,
+#     'BLACKLIST_AFTER_ROTATION': True,
+#     'UPDATE_LAST_LOGIN': False,
 
-    'ALGORITHM': 'HS256',
-    'SIGNING_KEY': os.environ.get('SECRET_JWT'),
-}
+#     'ALGORITHM': 'HS256',
+#     'SIGNING_KEY': os.environ.get('SECRET_JWT'),
+# }
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "file-upload-env.eba-bf76q3hm.us-east-1.elasticbeanstalk.com ",  "api.mathewcoolwebsite.com" ]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "file-upload-env.eba-bf76q3hm.us-east-1.elasticbeanstalk.com",  "api.mathewcoolwebsite.com" ]
 
 
 # Application definition
@@ -133,11 +141,11 @@ WSGI_APPLICATION = 'mario.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 SECRET_KEY = os.environ.get('SECRET_KEY')
 if not SECRET_KEY:
-    print("no secret key found from env, using temp value 5")
-    SECRET_KEY = 5
+    raise ValueError("SECRET_KEY not found in environment variables.")
+    
 
 
-DB_NAME = os.getenv("DB_NAME")
+DB_NAME = os.getenv("DB_NAME", "default_db")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
@@ -220,7 +228,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'bucket-file-system'  # Replace with your bucket name
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-AWS_S3_REGION_NAME = "us-east"
+AWS_S3_REGION_NAME = "us-east-1"
 # Cache control and file disposition
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
